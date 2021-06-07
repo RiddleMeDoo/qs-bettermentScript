1c0b6e73830851752de7e4a0ed13af116378e740
Kana Hashimoto <holy_tippin_cow@live.ca>
2021-06-07 16:58:48 -0400

    Update quests to use Promises

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         Queslar Testing Grounds
+// @name         Queslar Betterment Script
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  This is an attempt to do scripting stuff with Queslar
+// @description  A script that lets you know more info about quests
 // @author       RiddleMeDoo
-// @match        *test.queslar.com*
+// @match        */www.queslar.com*
+// @match        */queslar.com*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-class QTGScript {
+class Script {
   constructor() {
     // Get quest data
     this.quest = {
@@ -21,7 +22,7 @@ class QTGScript {
       minActions: 360,
       maxActions: 580,
     };
-    
+
     //observer stuff
     this.initObservers();
     this.currentPath = window.location.hash.split("/").splice(2).join();
@@ -29,6 +30,7 @@ class QTGScript {
 
   get gameData() { //ULTIMATE POWER
     //Dynamically get an update of the game's state
+    //* Sometimes doesn't load (I don't know why)
     let rootElement = getAllAngularRootElements()[0].children[1]["__ngContext__"][30];
     return rootElement.playerGeneralService;
   }
@@ -37,22 +39,32 @@ class QTGScript {
     //Couldn't find an easier method than doing a POST request
     this.gameData.httpClient.post('/player/load/misc', {}).subscribe(
       val => {this.quest.questsCompleted = val.playerMiscData.quests_completed},
-      response => console.log('QTG: POST request failure', response)
+      response => console.log('QBS: POST request failure', response)
     );
-    let questService = this.gameData.playerQuestService;
-    // //Great power comes with a cost
-    // //Wait for service to load
-    // while(!playerGeneralService.playerQuestService || !questService.refreshesUsed && !questService.refreshesBought) {
-    //   await new Promise(resolve => setTimeout(resolve, 1000));
-    // }
 
-    this.quest.numRefreshes = questService.refreshesBought + 5;
-    this.quest.refreshesUsed = questService.refreshesUsed;
-    if(this.gameData.playerVillageService.isInVillage) {
+    await this.updateRefreshes();
+    if(this.gameData?.playerVillageService?.isInVillage) {
       this.quest.villageBold = this.gameData.playerVillageService.strengths.bold.amount;
     }
     //Can't be bothered to calculate it accurately using all 4 stats
-    this.quest.baseStat = Math.min(15, this.gameData.playerStatsService.strength * 0.0025);
+    this.quest.baseStat = Math.min(15, this.gameData.playerStatsService?.strength * 0.0025);
+  }
+
+  async updateRefreshes() {
+    //Wait for service to load
+    let servicePromise = new Promise((resolve, reject) => {
+      if(this.gameData.playerQuestService !== undefined) {
+        resolve(this.gameData.playerQuestService);
+      } else reject();
+    });
+    servicePromise.then(questService => {
+      this.quest.numRefreshes = questService.refreshesBought + 5;
+      this.quest.refreshesUsed = questService.refreshesUsed;
+      console.log('QBS: quests updated', this.quest.refreshesUsed);
+    },
+    rejection => {
+      console.log('QBS: Quest didnt load');
+    });
   }
 
   initObservers() {
@@ -77,24 +89,24 @@ class QTGScript {
       this.personalQuestObserver.observe(target, {
         childList: true, subtree: true, attributes: false,
       });
-      await this.updateQuestData();
+      await this.updateRefreshes();
     }
   }
 
   handlePersonalQuest(mutation) {
     //Filter out any unneeded mutations
-    if(mutation.addedNodes.length < 1 || 
-      mutation.addedNodes[0].localName === 'mat-tooltip-component' || 
+    if(mutation.addedNodes.length < 1 ||
+      mutation.addedNodes[0].localName === 'mat-tooltip-component' ||
       mutation.addedNodes[0].nodeName === '#text') {
       return;
     }
     let questTable = mutation.target.tagName === 'TBODY' ? mutation.target : mutation.target.querySelector('tbody');
-    
+
     if(questTable) {
       let rowIndex = 0;
       let infoRow = null;
       let objective = 0;
-      
+
       //There are two states: active quest and no quest
       if(questTable.children.length > 2) {//No quest
         rowIndex = 2;
@@ -109,7 +121,7 @@ class QTGScript {
         const actionsDone = parseInt(objectiveElem.innerText.split(" ")[0]);
         objective = parseInt(objectiveElem.innerText.split(" ")[2]);
         infoRow = this.getQuestInfoElem(objective - actionsDone);
-        
+
         //Add information below table
         if(!document.getElementById('questExplanation')) {
           const questDiv = questTable.parentElement.parentElement;
@@ -121,13 +133,13 @@ class QTGScript {
         return;
       }
       const statRewardElem = questTable.children[rowIndex].children[2];
-      
+
       //Might as well parse these instead of using the heavy questService
       const statReward = parseInt(statRewardElem.innerText.split(" ")[0].replace(/,/g, ""));
       statRewardElem.innerText = `${statRewardElem.innerText} (${(objective/statReward).toFixed(3)})`;
-      
+
       //Add an extra row for extra quest info if there isn't one already
-      if(!document.getElementById('questInfoRow')) questTable.appendChild(infoRow); 
+      if(!document.getElementById('questInfoRow')) questTable.appendChild(infoRow);
     }
   }
 
@@ -156,7 +168,7 @@ class QTGScript {
     const date = new Date();
     const finishTime = new Date(date.getTime() + actionsNeeded * 6000).toLocaleTimeString('en-GB').match(/\d\d:\d\d/)[0];
     const finishPartyTime = new Date(date.getTime() + (actionsNeeded + 1440) * 6000).toLocaleTimeString('en-GB').match(/\d\d:\d\d/)[0];
-    const info = ['',`${this.refreshesUsed}/${this.quest.numRefreshes} refreshes used`, '',
+    const info = ['',`${this.quest.refreshesUsed}/${this.quest.numRefreshes} refreshes used`, '',
       `Finishes at ${finishTime} - ${finishPartyTime}`];
     let htmlInfo = '';
     for (let text of info) {
@@ -173,7 +185,7 @@ class QTGScript {
     const avg = (this.quest.minActions/stat.max + this.quest.maxActions/stat.min) / 2;
     const info = ['',
       `Max Ratio: ${(this.quest.minActions/stat.max).toFixed(3)}`,
-      `Avg ratio: ${(avg).toFixed(3)}`, 
+      `Avg ratio: ${(avg).toFixed(3)}`,
       `Min ratio: ${(this.quest.maxActions/stat.min).toFixed(3)}`
     ];
     let htmlInfo = '';
@@ -187,110 +199,33 @@ class QTGScript {
 }
 
 
-// class Quest {
-//   constructor() {
-//     this.questsCompleted = 0;
-//     this.numRefreshes = 0;
-//     this.refreshesUsed = 0;
-//     this.villageBold = 0;
-//     this.baseStat = 15;
-//     this.minActions = 360;
-//     this.maxActions = 580;
-//   }
-
-//   async updateData(playerGeneralService) {
-//     //Couldn't find an easier method than doing a POST request
-//     playerGeneralService.httpClient.post('/player/load/misc', {}).subscribe(
-//       val => {this.questsCompleted = val.playerMiscData.quests_completed},
-//       response => console.log('QTG: POST request failure', response)
-//     );
-//     let questService = playerGeneralService.playerQuestService;
-//     //Great power comes with a cost
-//     //Wait for service to load
-//     while(!playerGeneralService.playerQuestService || !questService.refreshesUsed && !questService.refreshesBought) {
-//       await new Promise(resolve => setTimeout(resolve, 1000));
-//     }
-
-//     this.numRefreshes = questService.refreshesBought + 5;
-//     this.refreshesUsed = questService.refreshesUsed;
-//     if(playerGeneralService.playerVillageService.isInVillage) {
-//       this.villageBold = playerGeneralService.playerVillageService.strengths.bold.amount;
-//     }
-//     //Can't be bothered to calculate it accurately using all 4 stats
-//     this.baseStat = Math.min(15, playerGeneralService.playerStatsService.strength * 0.0025);
-//   }
-
-//   setRefreshesUsed(numRefreshes) {
-//     this.refreshesUsed = numRefreshes;
-//   }
-
-//   getStatReward() {
-//     return {
-//       max: Math.round((this.questsCompleted/300+this.baseStat+22.75)*(1+this.villageBold*2/100)*1.09),
-//       min: Math.round((this.questsCompleted/300+this.baseStat+8.5)*(1+this.villageBold*2/100)*1.09),
-//     }
-//   }
-
-//   getQuestInfoElem(actionsNeeded) {
-//     let row = document.createElement('tr');
-
-//     const date = new Date();
-//     const finishTime = new Date(date.getTime() + actionsNeeded * 6000).toLocaleTimeString('en-GB').match(/\d\d:\d\d/)[0];
-//     const finishPartyTime = new Date(date.getTime() + (actionsNeeded + 1440) * 6000).toLocaleTimeString('en-GB').match(/\d\d:\d\d/)[0];
-//     const info = ['',`${this.refreshesUsed}/${this.numRefreshes} refreshes used`, '',
-//       `Finishes at ${finishTime} - ${finishPartyTime}`];
-//     let htmlInfo = '';
-//     for (let text of info) {
-//       htmlInfo += `<td>${text}</td>`
-//     }
-//     row.innerHTML = htmlInfo;
-//     row.id = 'questInfoRow';
-//     return row;
-//   }
-
-//   getRatioElem() {
-//     let row = document.createElement('tr');
-//     const stat = this.getStatReward();
-//     const avg = (this.minActions/stat.max + this.maxActions/stat.min) / 2;
-//     const info = ['',
-//       `Max Ratio: ${(this.minActions/stat.max).toFixed(3)}`,
-//       `Avg ratio: ${(avg).toFixed(3)}`, 
-//       `Min ratio: ${(this.maxActions/stat.min).toFixed(3)}`
-//     ];
-//     let htmlInfo = '';
-//     for (let text of info) {
-//       htmlInfo += `<td>${text}</td>`
-//     }
-//     row.innerHTML = htmlInfo;
-//     row.id = 'questInfoRow';
-//     return row;
-//   }
-// }
-
-
 // This is where the script starts
-var QTG = null;
-console.log('QTG: Init load');
-let QTGLoader = null;
+var QBS = null;
+console.log('QBS: Init load');
+let QBSLoader = null;
 let numAttempts = 30;
 
 window.addEventListener('load', () => { //Load the page first before setting up the script
-  QTGLoader = setInterval(setupScript, 3000);
+  QBSLoader = setInterval(setupScript, 3000);
 });
 
 async function setupScript() {
-  if(document.getElementById('profile-next-level') && QTG === null) {
-    QTG = new QTGScript();
-    console.log('QTG: The script has been loaded.');
+  if(document.getElementById('profile-next-level') && QBS === null) {
+    QBS = new Script();
+    console.log('QBS: The script has been loaded.');
 
     window.addEventListener('locationchange', function(){
-      QTG.handlePathChange();
+      QBS.handlePathChange();
     })
-    clearInterval(QTGLoader);
+    clearInterval(QBSLoader);
+    await QBS.updateQuestData();
   } else {
-    console.log('QTG: Loading failed. Trying again...');
+    console.log('QBS: Loading failed. Trying again...');
     numAttempts--;
-    if(numAttempts <= 0) clearInterval(QTGLoader); //Stop trying after a while
+    if(numAttempts <= 0) {
+      clearInterval(QBSLoader); //Stop trying after a while
+      console.log('QBS: Loading failed. Stopping...');
+    }
   }
 }
 
