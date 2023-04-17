0acd0b6f5ffa834ba1067aaedc5a214ad5e5bcd1
RiddleMeDoo <13726465+RiddleMeDoo@users.noreply.github.com>
2023-04-16 20:11:56 -0400

    Update html source file and README

--- complete-qs-betterment-script.user.js
+++ complete-qs-betterment-script.user.js
@@ -6,7 +6,7 @@
 // @author       RiddleMeDoo
 // @include      *queslar.com*
 // @require      https://code.jquery.com/jquery-3.6.3.slim.min.js
-// @resource     settingsMenu https://raw.githubusercontent.com/RiddleMeDoo/qs-bettermentScript/tooltips/tomeSettingsMenu.html
+// @resource     settingsMenu https://raw.githubusercontent.com/RiddleMeDoo/qs-bettermentScript/master/tomeSettingsMenu.html
 // @grant        GM_getResourceText
 // ==/UserScript==
 
